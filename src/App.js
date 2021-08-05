dc6677d809d76a09c950498451ebe6dded18bdd3
Eric Nguyen <erictn3@uci.edu>
2021-08-04 20:52:08 -0700

    connected form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+// importing components 
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-
+      <header>
+        Testu this out
+      </header>
+      <Form />
     </div>
   );
 }
