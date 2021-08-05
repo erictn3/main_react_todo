f8b66cf123b7763c8f488584652c20e05bb163e0
Eric Nguyen <erictn3@uci.edu>
2021-08-05 09:49:42 -0700

    todo component added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+// import Components
+import Todo from './Todo';
 const TodoList = () => {
   return (
     <div className="todo-container">
       <ul className="todo-list"></ul>
+      <Todo />
     </div>
   )
 }
